a259dc2803a6e6dff064353005c2677a5c802049
Camren Tinker <tin16004@byui.edu>
2019-12-14 01:39:02 -0700

    Finished everything and uploaded to the new repo

---
+++ WebAppSample/WebAppSample/Scripts/example3.js
@@ -0,0 +1,56 @@
+﻿function appendChildExample() {
+    element = document.getElementById('DomManipExampleDiv');    
+    var button = document.createElement('button');
+    button.className = "btn-info row mx-auto";
+    button.innerHTML = "Insert Before Example";
+    button.onclick = function insertBeforeExample() {
+        var button2 = document.createElement('button');
+        button2.className = "btn-info row mx-auto";
+        button2.innerHTML = "Remove Child Example";
+        button2.onclick = function removeChildExample() {
+            element.removeChild(button2);
+        }
+        element.insertBefore(button2, button);        
+    }
+    element.appendChild(button);
+}
+
+function HtmlCanvasExample() {
+    element = document.getElementById('CanvasExample');
+    context = element.getContext("2d");
+    var pos, vel;
+    pos = {
+        x: 10,
+        y: 10
+    };
+    vel = {
+        x: 1,
+        y: 1
+    };
+
+    var loop = function () {
+        context.clearRect(0, 0, element.width, element.height);
+        pos.x += vel.x;
+        pos.y += vel.y;
+        if (pos.x < 5 || pos.x > element.width - 5) {
+            vel.x *= -1;
+        }
+        if (pos.y < 5 || pos.y > element.height - 5) {
+            vel.y *= -1;
+        }
+        context.fillRect(pos.x - 5, pos.y - 5, 10, 10);
+    };
+
+    setInterval(loop, 1000 / 60);
+}
+
+function JsCssExample() {    
+    element = document.getElementById("JsCssExampleDiv");
+    element.addEventListener("click", function () {
+        element.classList.toggle("JsCssExampleAnimation");
+    });
+    element.addEventListener("mouseover", function () {
+        element.classList.toggle("JsCssExampleTransition");
+    });
+    
+}
---
+++ WebAppSample/WebAppSample/Scripts/example4.js
@@ -0,0 +1,22 @@
+﻿function onTouchExample() {
+    element = document.getElementById("OnTouchExampleDiv");
+    element.addEventListener("touchend", function () {
+        element.classList.toggle("JsCssExampleAnimation"), false;
+    });
+    element.addEventListener("touchstart", function () {
+        element.classList.toggle("JsCssExampleTransition"), false;
+    });
+
+}
+
+function onLoadExample() {
+    element = document.getElementById("OnLoadExample");
+    element.innerHTML = "<p>This paragraph was inserted by the onLoad JavaScript Event</p>";
+}
+
+function animationEventExample() {
+    element = document.getElementById("OnTouchExampleDiv");
+    element.addEventListener("animationstart", function () {
+        alert("You've triggered an animation after clicking the magic button! Congratulations!"), false;
+    });    
+}
